1ab0b48b1c3f5cc1a4407717f025189184a3abfd
Junior <eloidsjr@hotmail.com>
2019-06-07 20:03:05 -0300

    Asynchronous operations using Tasks (async, await)

--- SalesWebMvc/Services/DepartmentService.cs
+++ SalesWebMvc/Services/DepartmentService.cs
@@ -1,6 +1,8 @@
 ﻿using SalesWebMvc.Models;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;  // usado para usar o Task, que é para chamar as operações assíncronas
+using Microsoft.EntityFrameworkCore; // para usar o .ToListAsync
 
 
 namespace SalesWebMvc.Services
@@ -14,10 +16,20 @@ namespace SalesWebMvc.Services
             _context = context;
         }
 
+        /* Abaixo é um processamento Síncrono:
         public List<Department> FindAll()
         {
             return _context.Department.OrderBy(x => x.Name).ToList();
         }
+        */
+
+        //abaixo vamos fazer um processamento Assíncrono:
+        public async Task<List<Department>> FindAllAsync()
+        {
+            return await _context.Department.OrderBy(x => x.Name).ToListAsync();  // await avisa o compilador que a operação é assincrona
+        }
+
+
 
     }
 }
--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -17,29 +17,66 @@ namespace SalesWebMvc.Services
             _context = context;
         }
 
+        /*função sincrona:
         public List<Seller> FindAll()
         {
             return _context.Seller.ToList();
         }
+        */
 
+        
+        //Função assincrona:
+        public async Task<List<Seller>> FindAllAsync()
+        {
+            return await _context.Seller.ToListAsync();
+        }
+
+        /*Função sincrona:
         public void Insert(Seller obj)
         {
             _context.Add(obj);
             _context.SaveChanges();
         }
+        */
 
+        //função assincrona:
+        public async Task InsertAsync(Seller obj)
+        {
+            _context.Add(obj);
+            await _context.SaveChangesAsync();
+        }
+
+        /*função sincrona:
         public Seller FindById(int id)
         {
             return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
+        */
 
+        //função assincrona:
+        public async Task<Seller> FindByIdAsync(int id)
+        {
+            return await _context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(obj => obj.Id == id);
+        }
+
+        /*função sincrona:
         public void Remove(int id)
         {
             var obj = _context.Seller.Find(id);
             _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
+        */
 
+        //função assincrona:
+        public async Task RemoveAsync(int id)
+        {
+            var obj = await _context.Seller.FindAsync(id);
+            _context.Seller.Remove(obj);
+            await _context.SaveChangesAsync();
+        }
+
+        /*Função sincrona:
         public void Update(Seller obj)
         {
             if (!_context.Seller.Any(x => x.Id == obj.Id))
@@ -56,6 +93,27 @@ namespace SalesWebMvc.Services
                 throw new DbConcurrencyException(e.Message);
             }
         }
+        */
+
+            //função assincrona:
+        public async Task UpdateAsync(Seller obj)
+        {
+            bool hasAny = await _context.Seller.AnyAsync(x => x.Id == obj.Id);
+            if (!hasAny)
+            {
+                throw new NotFoundException("Id not found");
+            }
+            try
+            {
+                _context.Update(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+        }
+
 
     }
 }
