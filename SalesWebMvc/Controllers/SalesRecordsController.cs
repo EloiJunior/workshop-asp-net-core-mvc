f13426d1dadf9f08fd0e5f95749a6e7a5b998f82
Junior <eloidsjr@hotmail.com>
2019-06-08 16:32:26 -0300

    Preparing sales search navigation views

---
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SalesRecordsController : Controller   //por definição o nome do controler é o nome da classe de dominio no plural
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult SimpleSearch()
+        {
+            return View();
+        }
+
+
+        public IActionResult GroupingSearch()
+        {
+            return View();
+        }
+
+    }
+}
